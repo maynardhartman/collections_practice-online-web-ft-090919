386e8eeef1429430c8e0df06dcb55f7c26fbb66a
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-20 04:24:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
+def sort_array_asc(array)
+  ret_array = Array.new
+    ret_array << array.sort
+  puts ret_array
+  return ret_array
+end
 
+def sort_array_desc(array)
+  index = 0
+  
+  a = array[index]
+  b = array[index + 1]
+  if a < b #swap
+  }
+p array
+end 
